0227d4c3944a414155526c7d34ed943cca7beed5
Owen Shoemaker <you@example.com>
2015-04-07 15:43:21 -0700

    Forcing http/s for certain pages

--- routes/index.js
+++ routes/index.js
@@ -9,22 +9,22 @@ router.get('/', controllers.index );
 router.get('/index.:format', controllers.index);
 
 // Signup
-router.get('/signup.:format', controllers.signup);
-router.post('/signup.:format', controllers.signupPost);
+router.get('/signup.:format', httpHelpers.forceHttps, controllers.signup);
+router.post('/signup.:format', httpHelpers.forceHttps, controllers.signupPost);
 
 // Login
 router.get('/login.:format', httpHelpers.forceHttps, controllers.loginGet);
-router.post('/login.:format', controllers.loginPost);
+router.post('/login.:format', httpHelpers.forceHttps, controllers.loginPost);
 
 
-router.get('/sites/:site', controllers.site);
-router.get('/upload/igc', controllers.getIgcUpload);
-router.post('/upload/igc', controllers.postIgcUpload);
+router.get('/sites/:site', httpHelpers.forceHttp, controllers.site);
+router.get('/upload/igc', httpHelpers.forceHttps, controllers.getIgcUpload);
+router.post('/upload/igc', httpHelpers.forceHttps, controllers.postIgcUpload);
 
-router.get('/mm5', controllers.getMm5);
+router.get('/mm5', httpHelpers.forceHttp, controllers.getMm5);
 
-router.get('/blipspot/:site', controllers.getBlipspot);
+router.get('/blipspot/:site', httpHelpers.forceHttp, controllers.getBlipspot);
 
-router.get('/dashboard', controllers.dashboard);
+router.get('/dashboard', httpHelpers.forceHttps, controllers.dashboard);
 
 module.exports = router;
