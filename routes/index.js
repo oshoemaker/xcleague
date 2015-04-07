3f07f91342e8362b08e4ca9eafa45a8b88aded91
Owen Shoemaker <you@example.com>
2015-04-07 16:14:55 -0700

    Fixing a logic flaw

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,12 @@ router.post('/signup.:format', httpHelpers.forceHttps, controllers.signupPost);
 router.get('/login.:format', httpHelpers.forceHttps, controllers.loginGet);
 router.post('/login.:format', httpHelpers.forceHttps, controllers.loginPost);
 
-
-router.get('/sites/:site', httpHelpers.forceHttp, controllers.site);
+// IGC Upload
 router.get('/upload/igc', httpHelpers.forceHttps, controllers.getIgcUpload);
 router.post('/upload/igc', httpHelpers.forceHttps, controllers.postIgcUpload);
 
-router.get('/mm5', httpHelpers.forceHttp, controllers.getMm5);
+// Site information
+router.get('/sites/:site', httpHelpers.forceHttp, controllers.site);
 
 router.get('/blipspot/:site', httpHelpers.forceHttp, controllers.getBlipspot);
 
